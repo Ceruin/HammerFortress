af6a8b7ea0e69dd2c4ce48cd5456316fbc4c0d16
Ceruin <13532471+Ceruin@users.noreply.github.com>
2020-07-21 22:06:36 -0400

    AI Pathing

---
+++ HammerFortress/Assets/Scripts/EnemyGFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    public AIPath aipath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aipath.desiredVelocity.x > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);         
+        } else if (aipath.desiredVelocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+}
